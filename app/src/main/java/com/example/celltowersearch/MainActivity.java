99dc3d0beac50c4b806fc1e90d8f4c138aba7a9f
s21sd <srivastavasunny359@gmail.com>
2024-03-06 20:44:45 +0530

    sdfsdf

--- app/src/main/java/com/example/celltowersearch/MainActivity.java
+++ app/src/main/java/com/example/celltowersearch/MainActivity.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -16,43 +15,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-
-class CellTower{
-    String radio;
-    int mcc;
-    int  net;
-    int area;
-    int cell;
-    int unit;
-    double lon;
-    double lat;
-    int range;
-    int samples;
-    int changeable;
-    long created;
-    long updated;
-    int averageSignal;
-
-    public CellTower(String radio, int mcc, int net, int area, int cell, int unit,
-                     double lon, double lat, int range, int samples, int changeable,
-                     long created, long updated, int averageSignal) {
-        this.radio = radio;
-        this.mcc = mcc;
-        this.net = net;
-        this.area = area;
-        this.cell = cell;
-        this.unit = unit;
-        this.lon = lon;
-        this.lat = lat;
-        this.range = range;
-        this.samples = samples;
-        this.changeable = changeable;
-        this.created = created;
-        this.updated = updated;
-        this.averageSignal = averageSignal;
-    }
-}
 
 public class MainActivity extends AppCompatActivity {
     private EditText searchEditText;
@@ -68,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         searchEditText = findViewById(R.id.name);
         searchButton = findViewById(R.id.save);
 
-
         // First off all I want to read the data which is present in the csv file then from there ...
         latitudes = readCSVFile();
 
@@ -78,25 +39,75 @@ public class MainActivity extends AppCompatActivity {
                 String query = searchEditText.getText().toString().trim();
                 if (!query.isEmpty()) {
                     // Calling the Binary Search on the data present in the latitudes array list if present it will return the true otherwise false
+                    long startTime = System.currentTimeMillis();
                     boolean found = binarySearchLatitude(query);
+                    long endTime = System.currentTimeMillis();
                     if (found) {
-                        Toast.makeText(MainActivity.this, "Latitude " + query + " is present in the file", Toast.LENGTH_SHORT).show();
+                        double range = getRangeFromLatitude(query);
+                        Toast.makeText(MainActivity.this, "Latitude " + query + " is present in the file with range: " + range, Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(MainActivity.this, "Latitude " + query + " is not present in the file", Toast.LENGTH_SHORT).show();
                     }
+                    long timeTaken = endTime - startTime;
+                    Log.d("Search Time", "Time taken for search: " + timeTaken + " milliseconds");
                 } else {
                     Toast.makeText(MainActivity.this, "Please enter a latitude value to search", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
+//    private boolean binarySearchLatitude(String query) {
+//        int low = 0;
+//        int high = latitudes.size() - 1;
+//
+//        while (low <= high) {
+//            int mid = low + (high - low) / 2;
+//            double midValue = latitudes.get(mid);
+//
+//            if (midValue == Double.parseDouble(query)) {
+//                return true;
+//            } else if (midValue < Double.parseDouble(query)) {
+//                low = mid + 1;
+//            } else {
+//                high = mid - 1;
+//            }
+//        }
+//
+//        return false;
+//    }
 
-    // Binary Search Function to just check If my data is present or not
+
     private boolean binarySearchLatitude(String query) {
-
         Collections.sort(latitudes);
         int index = Collections.binarySearch(latitudes, Double.parseDouble(query));
         return index >= 0;
+    }
+
+    private double getRangeFromLatitude(String query) {
+        try {
+            InputStream inputStream = getAssets().open("cell_towers.csv");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+
+                if (data.length > 0) {
+                    try {
+                        double latitude = Double.parseDouble(data[7]);
+                        if (Double.parseDouble(query) == latitude) {
+                            return Double.parseDouble(data[8]);  // As We know We have 7 col is the lat so If range match we just give the corresponding range
+                        }
+                    } catch (NumberFormatException e) {
+                        Log.e("CSV Data", "Error parsing latitude: " + data[7]);
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 
     private ArrayList<Double> readCSVFile() {
@@ -113,9 +124,8 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         double latitude = Double.parseDouble(data[7]);
                         latitudes.add(latitude);
-                        Log.d("CSV Data", "Latitude: " + latitude);
+//                        Log.d("CSV Data", "Latitude: " + latitude);
                     } catch (NumberFormatException e) {
-
                         Log.e("CSV Data", "Error parsing latitude: " + data[7]);
                     }
                 }
