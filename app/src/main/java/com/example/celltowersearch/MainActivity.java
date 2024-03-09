61feca9269dbd54e7fee52bd91a8c72bedc0807d
s21sd <srivastavasunny359@gmail.com>
2024-03-09 12:37:23 +0530

    sdfsdf

--- app/src/main/java/com/example/celltowersearch/MainActivity.java
+++ app/src/main/java/com/example/celltowersearch/MainActivity.java
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -14,9 +11,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 class Tower {
-
     double latitude;
     double longitude;
     double mcc;
@@ -33,13 +31,20 @@ class Tower {
         this.lac = lac;
         this.cell = cell;
         this.range = range;
+    }
 
+    public static class DistanceRangePair {
+        public double distance;
+        public int range;
+
+        public DistanceRangePair(double distance, int range) {
+            this.distance=distance;
+            this.range= range;
+        }
     }
 }
 
 public class MainActivity extends AppCompatActivity {
-    private EditText searchEditText;
-    private Button searchButton;
     private ArrayList<Tower> towers;
 
     @Override
@@ -47,27 +52,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        searchEditText = findViewById(R.id.name);
-        searchButton = findViewById(R.id.save);
-
         towers = readCSVFile();
-                    double latitude = 20.3488139;
-                    double longitude = 85.8163151; // Example longitude, change as needed
-                    double[] location = {latitude, longitude};
-                    Tower nearestTower = findNearestTower(location, towers);
-                    if (nearestTower != null) {
-                        Log.d("Nearest Tower", "Latitude: " + nearestTower.latitude + " Log: " + nearestTower.longitude + " mcc: " + nearestTower.mcc + " mnc: " + nearestTower.mnc + " lac: " + nearestTower.lac + " cellid: " + nearestTower.cell + " range: " + nearestTower.range);
-                        Toast.makeText(MainActivity.this, "Nearest Tower: Latitude=" + nearestTower.latitude + ", Longitude=" + nearestTower.longitude, Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "No towers found", Toast.LENGTH_SHORT).show();
-                    }
+//        double latitude = 20.3488139;
+//        double longitude =85.8163151;
 
+        double latitude = 27.171894;
+        double longitude =81.210715 ;
+        double[] location = {latitude, longitude};
 
+        ArrayList<Tower.DistanceRangePair> pairs = findNearestTower(location, towers);
+        if (!pairs.isEmpty()) {
+            Log.d("Distance and index","Distance"+pairs.get(0).distance+ "Range "+pairs.get(0).range);
+            Log.d("Distance and index","Distance"+pairs.get(1).distance+ "Range "+pairs.get(1).range);
 
+//            for (Tower.DistanceRangePair pair : pairs) {
+//                Log.d("Distance and Range", "Distance: " + pair.distance + ", Range: " + pair.range);
+//                break;
+//            }
+        } else {
+            Toast.makeText(MainActivity.this, "No towers found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    static ArrayList<Tower.DistanceRangePair> findNearestTower(double[] location, ArrayList<Tower> towers) {
+        ArrayList<Tower.DistanceRangePair> pairs = new ArrayList<>();
+        int cnt=0;
+        for (Tower tower : towers) {
+            cnt++;
+            double distance = euclideanDistance(location[0], location[1], tower.latitude, tower.longitude);
+            int range =cnt ;
+            Tower.DistanceRangePair pair = new Tower.DistanceRangePair( distance, range);
+            pairs.add(pair);
+        }
+        Collections.sort(pairs, new Comparator<Tower.DistanceRangePair>() {
+            @Override
+            public int compare(Tower.DistanceRangePair pair1, Tower.DistanceRangePair pair2) {
+                // Compare based on distance
+                return Double.compare(pair1.distance, pair2.distance);
+            }
+        });
+        return pairs;
     }
 
     static double euclideanDistance(double lat1, double lon1, double lat2, double lon2) {
-        final double R = 6371; // Earth radius in kilometers
+        final double R = 6371;
 
         double dLat = Math.toRadians(lat2 - lat1);
         double dLon = Math.toRadians(lon2 - lon1);
@@ -80,30 +108,10 @@ public class MainActivity extends AppCompatActivity {
         return distance;
     }
 
-//    static double euclideanDistance(double x1, double y1, double x2, double y2) {
-//        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-//    }
-    static Tower findNearestTower(double[] location, ArrayList<Tower> towers) {
-        double minDistance = Double.POSITIVE_INFINITY;
-        Tower nearestTower = null;
-
-//        Toast.makeText(MainActivity.this, towers.size(), Toast.LENGTH_SHORT).show();
-        Log.d("Len","Length"+towers.size());
-        for (Tower tower : towers) {
-            double distance = euclideanDistance(location[0], location[1], tower.latitude, tower.longitude);
-            if (distance < minDistance) {
-                minDistance = distance;
-                nearestTower = tower;
-            }
-        }
-
-        return nearestTower;
-    }
-
     private ArrayList<Tower> readCSVFile() {
         ArrayList<Tower> towers = new ArrayList<>();
         try {
-            InputStream inputStream = getAssets().open("cell_towers.csv");
+            InputStream inputStream = getAssets().open("Datalacks.csv");
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line;
 
@@ -112,14 +120,13 @@ public class MainActivity extends AppCompatActivity {
 
                 if (data.length >= 9) { // Ensure that there are at least 9 columns
                     try {
-                        double latitude = Double.parseDouble(data[7]);
                         double mcc = Double.parseDouble(data[1]);
                         double mnc = Double.parseDouble(data[2]);
                         double lac = Double.parseDouble(data[3]);
                         double cell = Double.parseDouble(data[4]);
                         double range = Double.parseDouble(data[8]);
                         double longitude = Double.parseDouble(data[6]);
-
+                        double latitude = Double.parseDouble(data[7]);
                         towers.add(new Tower(latitude, longitude, mcc, mnc, lac, cell, range));
                     } catch (NumberFormatException e) {
                         Log.e("CSV Data", "Error parsing latitude or longitude: " + e.getMessage());
